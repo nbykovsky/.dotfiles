0ee906710d159e6b5fe3fb1f71b21ef3f6828c56
Nik <nikolay.bykovsky@gmail.com>
2022-02-11 20:59:12 +1100

    added nvim-tree

--- config/nvim/nvim.nix
+++ config/nvim/nvim.nix
@@ -7,9 +7,12 @@ in
   vimAlias = true;
   extraConfig = ''
     luafile ${path}/lua/user/options.lua
+    luafile ${path}/lua/user/keymaps.lua
+    luafile ${path}/lua/user/nvim-tree.lua
   '';
 
   plugins = with pkgs.vimPlugins; [
-
+    nvim-web-devicons
+    nvim-tree-lua
   ];
 }
