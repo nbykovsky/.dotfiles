80a8f8e4bdf6d9e392ff1039ca1a3bccb069ff82
Nik <nikolay.bykovsky@gmail.com>
2022-02-12 21:18:09 +1100

    added color scheme and plugings

--- config/nvim/nvim.nix
+++ config/nvim/nvim.nix
@@ -9,10 +9,16 @@ in
     luafile ${path}/lua/user/options.lua
     luafile ${path}/lua/user/keymaps.lua
     luafile ${path}/lua/user/nvim-tree.lua
+    luafile ${path}/lua/user/colorscheme.lua
   '';
 
   plugins = with pkgs.vimPlugins; [
+    popup-nvim
+    plenary-nvim
+    nvim-autopairs
+    comment-nvim
     nvim-web-devicons
     nvim-tree-lua
+    vim-code-dark
   ];
 }
