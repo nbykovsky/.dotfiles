2fe86210d6fba6f0b38f641cb97e4357af2cbd54
Nik Bykovsky <nikolay.bykovsky@gmail.com>
2022-04-06 21:25:40 +1000

    add vscode

--- users/nik/home.nix
+++ users/nik/home.nix
@@ -60,6 +60,18 @@ in
       core.editor = "nvim";
     };
   };
+
+  programs.vscode = {
+    enable = true;
+    #package = pkgs.vscodium;    # You can skip this if you want to use the unfree version
+    extensions = with pkgs.vscode-extensions; [
+      # Some example extensions...
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+      matklad.rust-analyzer
+    ];
+  };
   home.packages = with pkgs; [
     # neovim
     git
